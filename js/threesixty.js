015092737a1cbd74015e432c65da386691bb4963
Gabriel Poça <gabrielpoca@gmail.com>
2014-06-11 15:06:15 +0100

    Better javascript code.

--- js/threesixty.js
+++ js/threesixty.js
@@ -1,282 +1,206 @@
 (function() {
   $.fn.threesixty = function(image, frames) {
-    threesixty(this, image, frames)
+    new ThreeSixty().initialize(this, image, frames);
   };
 
-  var threesixty = function (container, image, imageTotalFrames) {
-    // Tells if the app is ready for user interaction
-    var ready = false,
-    // Tells the app if the user is dragging the pointer
-    dragging = false,
-    // Stores the pointer starting X position for the pointer tracking
-    pointerStartPosX = 0,
-    // Stores the pointer ending X position for the pointer tracking
-    pointerEndPosX = 0,
-    // Stores the distance between the starting and ending pointer X position in each time period we are tracking the pointer
-    pointerDistance = 0,
+  function ThreeSixty() {
+    this.ready = false,
+    this.dragging = false,
 
-    // The starting time of the pointer tracking period
-    monitorStartTime = 0,
+    this.pointerStartPosX = 0,
+    this.pointerEndPosX = 0,
+    this.monitorStartTime = 0,
+
     // The pointer tracking time duration
-    monitorInt = 10,
+    this.monitorInt = 10,
+
     // A setInterval instance used to call the rendering function
-    ticker = 0,
+    this.ticker = 0,
+
     // Sets the speed of the image sliding animation
-    speedMultiplier = -10,
-    // Stores the total amount of images we have in the sequence
-    totalFrames = 180,
-    // The current frame value of the image slider animation
-    currentFrame = 0,
-    // Stores all the loaded image objects
-    frames = [],
-    // The value of the end frame which the currentFrame will be tweened to during the sliding animation
-    endFrame = 0,
-    // We keep track of the loaded images by increasing every time a new image is added to the image slider
-    loadedImages = 0,
+    this.speedMultiplier = -10,
 
-    // Caching DOM element references
-    $document = $(document),
-    $container = undefined,
-    $images = undefined;
+    this.totalFrames = 180,
+    this.currentFrame = 0,
+    this.frames = [],
+    this.endFrame = 0,
+    this.loadedImages = 0,
 
-    function initialize(container, image, imageTotalFrames) {
-      $container = container;
-      $images = $('<ol>');
-      $container.append($images);
+    this.$document = $(document),
+    this.$container = undefined,
+    this.$images = undefined;
+  }
 
-      totalFrames = imageTotalFrames;
-      var tmpImage = new Image();
-      tmpImage.src = image;
-      $(tmpImage).load(function() {
-        var width = tmpImage.width;
-        var height = tmpImage.height;
+  ThreeSixty.prototype.initialize = function (container, image, imageTotalFrames) {
+    this.$container = container;
+    this.$images = $('<ol>');
+    this.$container.append(this.$images);
 
-        for(var i = 0; i < totalFrames; i++) {
-          loadImage(image, height, width / totalFrames, width / totalFrames * i);
-        }
-      });
-      setUserEvents();
+    this.totalFrames = imageTotalFrames;
+    var tmpImage = new Image();
+    tmpImage.src = image;
+    $(tmpImage).load(function() {
+      var width = tmpImage.width;
+      var height = tmpImage.height;
+      for(var i = 0; i < this.totalFrames; i++) {
+        this._loadImage(image, height, width / this.totalFrames, width / this.totalFrames * i);
+      }
+    }.bind(this));
+    this._setUserEvents();
+  }
+
+  ThreeSixty.prototype._loadImage = function(image, height, width, position) {
+    var li = $("<li>");
+
+    var styles = {
+      'background-image': 'url(' + image + ')',
+      'width': width + 'px',
+      'height': height + 'px',
+      'background-position': '-' + new String(position) + 'px 0'
     }
 
-    /**
-    * Creates a new <li> and loads the image.
-    */
-    function loadImage(image, height, width, position) {
-      var li = $("<li>");
+    li.css(styles)
+    li.addClass("previous-image");
 
-      var styles = {
-        'background-image': 'url(' + image + ')',
-        'width': width + 'px',
-        'height': height + 'px',
-        'background-size': 'auto',
-        'background-repeat': 'no-repeat',
-        'background-position': '-' + new String(position) + 'px 0'
-      }
+    this.frames.push(li);
+    this.$images.prepend(li);
 
-      li.css(styles)
-      li.addClass("previous-image");
+    this.loadedImages++;
+    if (this.loadedImages == this.totalFrames) {
+      this.frames[0].removeClass("previous-image").addClass("current-image");
+      this._showThreesixty();
+    }
+  };
 
-      frames.push(li);
-      $images.prepend(li);
+  ThreeSixty.prototype._showThreesixty = function() {
+    this.$images.fadeIn("slow");
+    this.ready = true;
+    this.endFrame = 0;
+    this._refresh();
+  };
 
-      loadedImages++;
-      if (loadedImages == totalFrames) {
-        frames[0].removeClass("previous-image").addClass("current-image");
-        showThreesixty();
-      }
-    };
-
-    /**
-    * Displays the images with the "swooshy" spinning effect.
-    * At this point it also sets the application to be ready for the user interaction.
-    */
-    function showThreesixty () {
-      // Fades in the image slider by using the jQuery "fadeIn" method
-      $images.fadeIn("slow");
-      // Sets the "ready" variable to true, so the app now reacts to user interaction 
-      ready = true;
-      // Sets the endFrame to an initial value...
-      endFrame = 0;
-      refresh();
-    };
-
-    /**
-    * Renders the image slider frame animations.
-    */
-    function render () {
-      // The rendering function only runs if the "currentFrame" value hasn't reached the "endFrame" one
-      if(currentFrame !== endFrame)
-      {	
-        /*
-        Calculates the 10% of the distance between the "currentFrame" and the "endFrame".
-        By adding only 10% we get a nice smooth and eased animation.
-        If the distance is a positive number, we have to ceil the value, if its a negative number, we have to floor it to make sure
-        that the "currentFrame" value surely reaches the "endFrame" value and the rendering doesn't end up in an infinite loop.
-        */
-        var frameEasing = endFrame < currentFrame ? Math.floor((endFrame - currentFrame) * 0.1) : Math.ceil((endFrame - currentFrame) * 0.1);
-        // Sets the current image to be hidden
-        hidePreviousFrame();
-        // Increments / decrements the "currentFrame" value by the 10% of the frame distance
-        currentFrame += frameEasing;
-        // Sets the current image to be visible
-        showCurrentFrame();
-      } else {
-        // If the rendering can stop, we stop and clear the ticker
-        window.clearInterval(ticker);
-        ticker = 0;
-      }
-    };
-
-    /**
-    * Creates a new setInterval and stores it in the "ticker"
-    * By default I set the FPS value to 60 which gives a nice and smooth rendering in newer browsers
-    * and relatively fast machines, but obviously it could be too high for an older architecture.
-    */
-    function refresh () {
-      // If the ticker is not running already...
-      if (ticker === 0) {
-        // Let's create a new one!
-        ticker = self.setInterval(render, Math.round(1000 / 60));
-      }
-    };
-
-    /**
-    * Hides the previous frame
-    */
-    function hidePreviousFrame() {
+  ThreeSixty.prototype._render = function() {
+    // The rendering function only runs if the "currentFrame" value hasn't reached the "endFrame" one
+    if(this.currentFrame !== this.endFrame) {
       /*
-      Replaces the "current-image" class with the "previous-image" one on the image.
-      It calls the "getNormalizedCurrentFrame" method to translate the "currentFrame" value to the "totalFrames" range (1-180 by default).
+      Calculates the 10% of the distance between the "currentFrame" and the "endFrame".
+      By adding only 10% we get a nice smooth and eased animation.
+      If the distance is a positive number, we have to ceil the value, if its a negative number, we have to floor it to make sure
+      that the "currentFrame" value surely reaches the "endFrame" value and the rendering doesn't end up in an infinite loop.
       */
-      frames[getNormalizedCurrentFrame()].removeClass("current-image").addClass("previous-image");
-    };
+      var frameEasing = this.endFrame < this.currentFrame ? Math.floor((this.endFrame - this.currentFrame) * 0.1) : Math.ceil((this.endFrame - this.currentFrame) * 0.1);
+      // Sets the current image to be hidden
+      this._hidePreviousFrame();
+      // Increments / decrements the "currentFrame" value by the 10% of the frame distance
+      this.currentFrame += frameEasing;
+      // Sets the current image to be visible
+      this._showCurrentFrame();
+    } else {
+      // If the rendering can stop, we stop and clear the ticker
+      window.clearInterval(this.ticker);
+      this.ticker = 0;
+    }
+  };
 
-    /**
-    * Displays the current frame
-    */
-    function showCurrentFrame() {
-      /*
-      Replaces the "current-image" class with the "previous-image" one on the image.
-      It calls the "getNormalizedCurrentFrame" method to translate the "currentFrame" value to the "totalFrames" range (1-180 by default).
-      */
-      frames[getNormalizedCurrentFrame()].removeClass("previous-image").addClass("current-image");
-    };
+  /**
+  * Creates a new setInterval and stores it in the "ticker"
+  * By default I set the FPS value to 60 which gives a nice and smooth rendering in newer browsers
+  * and relatively fast machines, but obviously it could be too high for an older architecture.
+  */
+  ThreeSixty.prototype._refresh = function() {
+    // If the ticker is not running already...
+    if (this.ticker === 0) {
+      // Let's create a new one!
+      this.ticker = self.setInterval(function() { this._render() }.bind(this), Math.round(1000 / 60));
+    }
+  };
 
-    /**
-    * Returns the "currentFrame" value translated to a value inside the range of 0 and "totalFrames"
-    */
-    function getNormalizedCurrentFrame() {
-      var c = -Math.ceil(currentFrame % totalFrames);
-      if (c < 0) c += (totalFrames - 1);
-      return c;
-    };
+  ThreeSixty.prototype._hidePreviousFrame = function() {
+    this.frames[this._getNormalizedCurrentFrame()].removeClass("current-image").addClass("previous-image");
+  };
 
-    /**
-    * Returns a simple event regarding the original event is a mouse event or a touch event.
-    */
-    function getPointerEvent(event) {
-      return event.originalEvent.targetTouches ? event.originalEvent.targetTouches[0] : event;
-    };
+  ThreeSixty.prototype._showCurrentFrame = function() {
+    this.frames[this._getNormalizedCurrentFrame()].removeClass("previous-image").addClass("current-image");
+  };
 
-    function setUserEvents() {
+  /**
+  * Returns the "currentFrame" value translated to a value inside the range of 0 and "totalFrames"
+  */
+  ThreeSixty.prototype._getNormalizedCurrentFrame = function() {
+    var c = -Math.ceil(this.currentFrame % this.totalFrames);
+    if (c < 0) c += (this.totalFrames - 1);
+    return c;
+  };
 
-      /**
-      * Adds the jQuery "mousedown" event to the image slider wrapper.
-      */
-      $container.on("mousedown", function (event) {
-        // Prevents the original event handler behaciour
-        event.preventDefault();
-        // Stores the pointer x position as the starting position
-        pointerStartPosX = getPointerEvent(event).pageX;
-        // Tells the pointer tracking function that the user is actually dragging the pointer and it needs to track the pointer changes
-        dragging = true;
-      });
+  /**
+  * Returns a simple event regarding the original event is a mouse event or a touch event.
+  */
+  ThreeSixty.prototype._getPointerEvent = function(event) {
+    return event.originalEvent.targetTouches ? event.originalEvent.targetTouches[0] : event;
+  };
 
-      /**
-      * Adds the jQuery "mouseup" event to the document. We use the document because we want to let the user to be able to drag
-      * the mouse outside the image slider as well, providing a much bigger "playground".
-      */
-      $document.on("mouseup", function (event){
-        // Prevents the original event handler behaciour
-        event.preventDefault();
-        // Tells the pointer tracking function that the user finished dragging the pointer and it doesn't need to track the pointer changes anymore
-        dragging = false;
-      });
+  ThreeSixty.prototype._setUserEvents = function() {
+    this.$container.on("mousedown", function (event) {
+      event.preventDefault();
+      this.pointerStartPosX = this._getPointerEvent(event).pageX;
+      this.dragging = true;
+    }.bind(this));
 
-      /**
-      * Adds the jQuery "mousemove" event handler to the document. By using the document again we give the user a better user experience
-      * by providing more playing area for the mouse interaction.
-      */
-      $document.on("mousemove", function (event){
-        // Prevents the original event handler behaciour
-        event.preventDefault();
-        // Starts tracking the pointer X position changes
-        trackPointer(event);
-      });
+    this.$document.on("mouseup", function (event){
+      event.preventDefault();
+      this.dragging = false;
+    }.bind(this));
 
-      /**
-      *
-      */
-      $container.on("touchstart", function (event) {
-        // Prevents the original event handler behaciour
-        event.preventDefault();
-        // Stores the pointer x position as the starting position
-        pointerStartPosX = getPointerEvent(event).pageX;
-        // Tells the pointer tracking function that the user is actually dragging the pointer and it needs to track the pointer changes
-        dragging = true;
-      });
+    this.$document.on("mousemove", function (event){
+      event.preventDefault();
+      this._trackPointer(event);
+    }.bind(this));
 
-      /**
-      *
-      */
-      $container.on("touchmove", function (event) {
-        // Prevents the original event handler behaciour
-        event.preventDefault();
-        // Starts tracking the pointer X position changes
-        trackPointer(event);
-      });
+    this.$container.on("touchstart", function (event) {
+      event.preventDefault();
+      this.pointerStartPosX = this._getPointerEvent(event).pageX;
+      this.dragging = true;
+    }.bind(this));
 
-      /**
-      *
-      */
-      $container.on("touchend", function (event) {
-        // Prevents the original event handler behaciour
-        event.preventDefault();
-        // Tells the pointer tracking function that the user finished dragging the pointer and it doesn't need to track the pointer changes anymore
-        dragging = false;
-      });
-    };
+    this.$container.on("touchmove", function (event) {
+      event.preventDefault();
+      this._trackPointer(event);
+    }.bind(this));
 
-    /**
-    * Tracks the pointer X position changes and calculates the "endFrame" for the image slider frame animation.
-    * This function only runs if the application is ready and the user really is dragging the pointer; this way we can avoid unnecessary calculations and CPU usage.
-    */
-    function trackPointer(event) {
-      var userDragging = ready && dragging ? true : false;
+    this.$container.on("touchend", function (event) {
+      event.preventDefault();
+      this.dragging = false;
+    }.bind(this));
+  };
 
-      if(userDragging) {
+  /**
+  * Tracks the pointer X position changes and calculates the "endFrame" for the image slider frame animation.
+  * This function only runs if the application is ready and the user really is dragging the pointer; this way we can avoid unnecessary calculations and CPU usage.
+  */
+  ThreeSixty.prototype._trackPointer = function(event) {
+    var userDragging = this.ready && this.dragging ? true : false;
 
-        // Stores the last x position of the pointer
-        pointerEndPosX = getPointerEvent(event).pageX;
+    if(userDragging) {
 
-        // Checks if there is enough time past between this and the last time period of tracking
-        if(monitorStartTime < new Date().getTime() - monitorInt) {
-          // Calculates the distance between the pointer starting and ending position during the last tracking time period
-          pointerDistance = pointerEndPosX - pointerStartPosX;
-          // Calculates the endFrame using the distance between the pointer X starting and ending positions and the "speedMultiplier" values
-          endFrame = currentFrame + Math.ceil((totalFrames - 1) * speedMultiplier * (pointerDistance / $container.width()));
-          // Updates the image slider frame animation
-          refresh();
-          // restarts counting the pointer tracking period
-          monitorStartTime = new Date().getTime();
-          // Stores the the pointer X position as the starting position (because we started a new tracking period)
+      // Stores the last x position of the pointer
+      this.pointerEndPosX = this._getPointerEvent(event).pageX;
 
-          pointerStartPosX = getPointerEvent(event).pageX;
-        }
-      } else {
-        return;
+      // Checks if there is enough time past between this and the last time period of tracking
+      if(this.monitorStartTime < new Date().getTime() - this.monitorInt) {
+        // Calculates the distance between the pointer starting and ending position during the last tracking time period
+        var pointerDistance = this.pointerEndPosX - this.pointerStartPosX;
+        // Calculates the endFrame using the distance between the pointer X starting and ending positions and the "speedMultiplier" values
+        this.endFrame = this.currentFrame + Math.ceil((this.totalFrames - 1) * this.speedMultiplier * (pointerDistance / this.$container.width()));
+        // Updates the image slider frame animation
+        this._refresh();
+        // restarts counting the pointer tracking period
+        this.monitorStartTime = new Date().getTime();
+        // Stores the the pointer X position as the starting position (because we started a new tracking period)
+
+        this.pointerStartPosX = this._getPointerEvent(event).pageX;
       }
-    };
-    initialize(container, image, imageTotalFrames);
+    } else {
+      return;
+    }
   };
 })();
